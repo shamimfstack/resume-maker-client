39063b59387685d78cfa31738a2681f07e6620d5
Shamim Ahammad <shamimfstack@gmail.com>
2023-12-14 22:17:02 +0600

    basic setup

---
+++ src/Pages/ResumeForm/ResumeForm.jsx
@@ -0,0 +1,11 @@
+
+
+const ResumeForm = () => {
+  return (
+    <div>
+      <h2>Resume Form</h2>
+    </div>
+  );
+}
+
+export default ResumeForm;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../MainLayout/MainLayout";
+import Home from "../Pages/Home/Home/Home";
+
+
+const Routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path:'/',
+                element: <Home></Home>
+            }
+        ]
+    }
+])
+
+export default Routes;
